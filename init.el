a00767b4ed3bf0e127983bbd1dbd77438d26fb65
motform <love.lagerkvist@protonmail.com>
2021-04-16 11:23:45 +0200

    Right align task-related modeline segments

--- init.el
+++ init.el
@@ -140,19 +140,16 @@
                                       '(tool-bar-lines . 0))))
 
 
-(setq-default column-number-mode t
-              mode-line-format   '("%e"
-                                   mode-line-front-space
-                                   mode-line-modified
-                                   mode-line-remote
-                                   mode-line-frame-identification
-                                   mode-line-buffer-identification
-                                   "   "
-                                   "%l:%c"
-                                   "  "
-                                   mode-line-misc-info
-                                   mode-line-end-spaces))
-
+(defun simple-mode-line-render (left right)
+  "Return a string of `window-width' length.
+Containing LEFT, and RIGHT aligned respectively."
+  (let ((available-width
+         (- (window-total-width)
+            (+ (length (format-mode-line left))
+               (length (format-mode-line right))))))
+    (append left
+            (list (format (format "%%%ds" available-width) ""))
+            right)))
 
 ;; Display to mode-line buffer name relative to current-project.
 ;; SOURCE: https://www.reddit.com/r/emacs/comments/8xobt3/tip_in_modeline_show_buffer_file_path_relative_to/
@@ -166,6 +163,21 @@
                                     (prj-parent           (file-name-directory (directory-file-name (expand-file-name prj)))))
                           (concat (file-relative-name (file-name-directory buffer-file-truename) prj-parent) (file-name-nondirectory buffer-file-truename)))
                         "%b"))))))
+
+(setq-default column-number-mode t
+              mode-line-format   '((:eval (simple-mode-line-render '("%e" ; left side
+                                                                     mode-line-front-space
+                                                                     mode-line-modified
+                                                                     mode-line-remote
+                                                                     mode-line-frame-identification
+                                                                     mode-line-buffer-identification
+                                                                     "   "
+                                                                     "%l:%c")
+                                                                   '(mode-line-misc-info  ; right side
+                                                                     "  "
+                                                                     mode-line-process
+                                                                     mode-line-end-spaces
+                                                                     cider-mode-line)))))
 
 
 
